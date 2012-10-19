75637ab42f43bad4805bbed5ad15779da8f3fdce
Catalyst IT Services <jburley@catalystitservices.com>
2012-10-19 10:43:36 -0700

    Fixed ex01 bug in ch04, added ex02.pl file.

--- ch04_subroutines/ex01.pl
+++ ch04_subroutines/ex01.pl
@@ -1,17 +1,20 @@
 #!/usr/bin/perl
 use 5.010;
 
+#prints the total of a list of preset numbers
+
 my @fred = qw(1 3 5 7 9);
 my $fred_total = &total(@fred);
 
 print "The total of \@fred is $fred_total.\n";
 
+#prints the total of a list of numbers entered by the user
 print "Enter some numbers on separate lines: \n";
-my @user_total = &total(<STDIN>);
+my $user_total = total(<STDIN>);
 print "The total of those numbers is $user_total.\n";
 
 sub total{
-	state $my_sum = 0;
+	my $my_sum = 0;
 	foreach $value (@_) {
 		$my_sum += $value;
 	}
---
+++ ch04_subroutines/ex02.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+#prints the sum of the number from 1-1000
+my @numberList = (1..1000);
+
+my $numberListTotal = &total(@numberList);
+print "The total from Way#1 is : $numberListTotal.\n";
+
+my $numberListTotal = &anotherTotal(@numberList);
+print "The toal from Way#2 is : $numberListTotal.\n";
+
+#One way to do it (verbose)
+sub total {
+	my $sum = 0;
+	foreach $number (@numberList) {
+		$sum += $number;
+	}
+	return $sum;
+}
+
+#Another less verbose way
+sub anotherTotal {
+	my $sum = 0;
+	foreach (@_) {
+		$sum += $_;
+	}
+	return $sum;
+}
